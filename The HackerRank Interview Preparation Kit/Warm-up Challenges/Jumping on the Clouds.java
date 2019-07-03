8edb842f23bd50603eae274a09fe836ece06f3e6
Sara Zaia <sarazaiaaa@gmail.com>
2019-07-03 17:40:38 +0100

    Add files via upload

---
+++ The HackerRank Interview Preparation Kit/Warm-up Challenges/Jumping on the Clouds.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the jumpingOnClouds function below.
+    static int jumpingOnClouds(int[] c) {
+
+        int index  = 0;
+        int result = 0;
+
+        while (index < c.length) {
+            if ((index+2) < c.length && c[index+2] != 1 ) {
+                System.out.println("INDEX " + index);
+                index += 2; 
+                result++;
+            } else {
+                index +=1;
+                result++;
+                
+            }
+
+        }
+        return result-1;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] c = new int[n];
+
+        String[] cItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int cItem = Integer.parseInt(cItems[i]);
+            c[i] = cItem;
+        }
+
+        int result = jumpingOnClouds(c);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
